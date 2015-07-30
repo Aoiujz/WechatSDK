e688de690575e29970ad82aa45e6b8390f12b356
麦当苗儿 <zuojiazi@topthink.net>
2015-07-30 22:15:11 +0800

    回复音乐消息

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -121,9 +121,9 @@ class IndexController extends Controller{
                         $wechat->replyMusic(
                             'Wakawaka!', 
                             'Shakira - Waka Waka, MaxRNB - Your first R/Hiphop source', 
-                            'http://yinyueshiting.baidu.com/data2/music/134368704/23473715212400128.mp3?xcode=59c2f8634e5593386f333bb99c6701d9', 
-                            'http://yinyueshiting.baidu.com/data2/music/134368704/23473715212400128.mp3?xcode=59c2f8634e5593386f333bb99c6701d9', 
-                            'http://pic6.qiyipic.com/thumb/20121218/a197952u2_160_90.jpg'
+                            'http://wechat.zjzit.cn/Public/music.mp3', 
+                            'http://wechat.zjzit.cn/Public/music.mp3', 
+                            'http://wechat.zjzit.cn/Public/music.jpg'
                         ); //回复音乐消息
                         break;
 
